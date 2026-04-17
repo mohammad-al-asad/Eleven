bbf697198a4484bc16e06c030e2838b6c2596ce2
mohammad-al-asad <maasad11914@gmail.com>
2026-04-18 05:56:28 +0600

    new api url added

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -1,3 +1,3 @@
-export const imgUrl = "https://eleven-lqw3.onrender.com";
+export const imgUrl = "https://api.stepelevenapp.org";
 export const url = `${imgUrl}/api/v1/`;
 export const getBaseUrl = () => url;
